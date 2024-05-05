901bb62bffca50cf2b59e30baaaba50c402ed91d
lvinyl <149749754+lvinyl@users.noreply.github.com>
2024-05-05 13:58:53 -0700

    fixed mixin

--- src/client/java/me/lvinyl/mixin/client/HealthThresholdClientMixin.java
+++ src/client/java/me/lvinyl/mixin/client/HealthThresholdClientMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
+public class HealthThresholdClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftClient.run()V
